3cb660d0239ebc391e82162d7f7a559532dea1d0
Prabhat Mani Srivastava <prabhatmani933@gmail.com>
2022-11-09 07:29:32 +0530

    Completed home page. Now to add context for Cart.

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,6 +3,8 @@ import { faker } from "@faker-js/faker";
 import SingleProduct from "./SingleProduct";
 import "./styles.css";
 
+faker.seed(100);
+
 const Home = () => {
   const productsArray = [...Array(20)].map(() => ({
     id: faker.datatype.uuid(),
@@ -11,12 +13,14 @@ const Home = () => {
     image: faker.image.business(),
   }));
 
-  const [products, setProducts] = useState(productsArray);
+  const [cart, setCart] = useState([]);
 
+  const [products] = useState(productsArray);
+  console.log(cart);
   return (
     <div className="productContainer">
-      {products.map((prod) => (
-        <SingleProduct prod={prod} />
+      {products.map((prod, idx) => (
+        <SingleProduct key={idx} prod={prod} cart={cart} setCart={setCart} />
       ))}
     </div>
   );
